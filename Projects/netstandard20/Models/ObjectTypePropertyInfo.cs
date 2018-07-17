4326ade297aab1610f286831d682af79b9196318
Casey Anderson <caseya@bluecats.com>
2018-07-17 15:51:00 -0500

    Small model fix

--- Projects/netstandard20/Models/ObjectTypePropertyInfo.cs
+++ Projects/netstandard20/Models/ObjectTypePropertyInfo.cs
@@ -6,7 +6,7 @@
         public string DisplayName { get; set; }
         public string Type { get; set; }
         public bool Required { get; set; }
-        public bool Editable { get; set; }
+        public bool? Editable { get; set; }
         public bool? Populated { get; set; }
         public string TimeCol { get; set; }
         public int? Length { get; set; }
