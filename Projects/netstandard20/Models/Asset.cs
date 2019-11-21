ea037d45003bff98156634671b997fbb53e24697
theporkchopexpress <darren@bluecats.com>
2019-11-21 17:44:21 +1100

    assets and associations

---
+++ Projects/netstandard20/Models/Asset.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BC.Loop.Api.Client.Models {
+
+    public class Asset {
+
+        public String ID { get; set; }
+        public String GroupID { get; set; }
+        public String ObjectType { get; set; }
+        public String TagID { get; set; }
+        public String EddystoneUID { get; set; }
+        public String AssetIdentifier { get; set; }
+        public String Description { get; set; }
+        public List<Association> Associations { get; set; }
+
+        public Asset() {
+            Associations = new List<Association>();
+        }
+
+    }
+
+}
---
+++ Projects/netstandard20/Models/Association.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BC.Loop.Api.Client.Models {
+
+    public class Association {
+
+        public String IdentifierType { get; set; }
+        public String Identifier { get; set; }
+
+    }
+
+}
